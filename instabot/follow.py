663e9e5b098a80bf0931d256fec3092777c89f56
peer <peerdenhoed@hotmail.nl>
2023-04-23 20:10:15 +0200

    Update follow.py

--- instabot/follow.py
+++ instabot/follow.py
@@ -7,6 +7,7 @@ from typing import Any, List, Tuple, Dict
 import os
 from .like_media import like_recent_posts
 import threading
+from .utils import load_config
 
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s',
@@ -20,6 +21,7 @@ logging.basicConfig(
 logger = logging.getLogger()
 
 def save_followed_user(cl: Client, user_id: int) -> None:
+    """Save followed user ID and timestamp to a file."""
     followed_users_folder = "followed_users"
     os.makedirs(followed_users_folder, exist_ok=True)
     file_path = os.path.join(followed_users_folder, f"followed_users_{cl.user_id}.txt")
@@ -28,6 +30,7 @@ def save_followed_user(cl: Client, user_id: int) -> None:
         file.write(f"{user_id},{datetime.datetime.now()}\n")
 
 def load_followed_users(cl: Client) -> List[Tuple[int, datetime.datetime]]:
+    """Load followed users' IDs and timestamps from a file."""
     followed_users = []
     followed_users_folder = "followed_users"
     os.makedirs(followed_users_folder, exist_ok=True)
@@ -41,10 +44,12 @@ def load_followed_users(cl: Client) -> List[Tuple[int, datetime.datetime]]:
     return followed_users
 
 def filter_users_to_unfollow(followed_users: List[Tuple[int, datetime.datetime]], follow_time: int) -> List[int]:
+    """Filter users that should be unfollowed based on follow_time."""
     now = datetime.datetime.now()
     return [user for user, timestamp in followed_users if (now - timestamp).total_seconds() >= follow_time]
 
 def remove_unfollowed_user(cl: Client, user: int) -> None:
+    """Remove unfollowed user from the followed users file."""
     followed_users = load_followed_users(cl)
     followed_users.remove(user)
 
@@ -57,6 +62,7 @@ def remove_unfollowed_user(cl: Client, user: int) -> None:
             file.write(f"{user}\n")
 
 def unfollow_users(cl: Client, unfollow_after: int) -> None:
+    """Unfollow users after a specified time."""
     logger.info("Started unfollowing process")
     followed_users = load_followed_users(cl)
     users_to_unfollow = filter_users_to_unfollow(followed_users, follow_time=unfollow_after)
@@ -67,34 +73,52 @@ def unfollow_users(cl: Client, unfollow_after: int) -> None:
         logger.info(f"Sleeping for {sleep_time} before unfollowing {user}")
         time.sleep(sleep_time)
         cl.user_unfollow(user)
-        logger.info(f"Unfollowed user {user.username}")
-        remove_unfollowed_user(cl, user=user)
+        logger.info(f"Unfollowed user {user}")
+
+def user_not_followed_before(cl: Client, user_id: int) -> bool:
+    """Check if the given user_id has been followed before."""
+    followed_users = load_followed_users(cl)
+    for user, _ in followed_users:
+        if user == user_id:
+            return False
+    return True
+
+def follow_user(cl: Client, user_id: int, engagement: Dict[str, Any]) -> bool:
+    """Follow a user and add it to followed_users file."""
+
+    if user_not_followed_before(cl, user_id):
+        cl.user_follow(user_id)
+        save_followed_user(cl, user_id=user_id)
+
+        if engagement["like_recent_posts"]:
+            like_recent_posts(cl, user_id=user_id, engagement=engagement)
+
+        logger.info(f"Followed user: {user_id}")
+
+        return True
+    logger.info("User was followed before, skipped")
+
+    return False
+
 
 def follow_user_followers(cl: Client, source_account: str, follows_per_day: int, engagement: Dict[str, Any]) -> None:
+    """Follow the followers of the source account and engage with their content."""
     logger.info("Started following user followers process..")
 
     user_id = cl.user_id_from_username(source_account)
-
     average_sleep_time = 86400 / follows_per_day
-    min_sleep_time = average_sleep_time * 0.5  # 50% less than the average
-    max_sleep_time = average_sleep_time * 1.5  # 50% more than the average
+    min_sleep_time = average_sleep_time * 0.5
+    max_sleep_time = average_sleep_time * 1.5
 
     users = cl.user_followers(user_id, True, amount=follows_per_day)
 
     for user in users:
         try:
-            logger.info(f"Following user: {user}")
-            cl.user_follow(user)
             sleep_time = random.uniform(min_sleep_time, max_sleep_time)
-            save_followed_user(cl, user_id=user)
-
-            if engagement["like_recent_posts"]:
-                like_recent_posts(cl, user_id=user, engagement=engagement)
-
-            logger.info(f"Sleeping for {sleep_time} seconds before following next user..")
-            time.sleep(sleep_time)
+            if follow_user(cl, user, engagement):
+                logger.info(f"Sleeping for {sleep_time} seconds before following next user..")
+                time.sleep(sleep_time)
 
         except Exception as e:
             logger.error(f"Error while processing user {user}: {e}")
             continue
-
