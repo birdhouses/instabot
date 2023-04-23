5273d7a31c3156274703b65a81ae2f3f8a8303cc
peer <peerdenhoed@hotmail.nl>
2023-04-23 02:59:35 +0200

    Add proxy support

--- instabot/utils.py
+++ instabot/utils.py
@@ -12,23 +12,68 @@ from instagrapi.exceptions import (
 from typing import Union
 import time
 import json
+import random
+import uuid
 
 def load_config(file_path: str) -> dict:
     with open(file_path, 'r') as file:
         return json.load(file)
 
-def update_client_settings(settings: dict) -> None:
-    # Implement the function to update the client settings.
-    pass
+def update_client_settings(client: Client, settings: dict) -> bool:
+    return client.set_settings(settings)
 
-def rebuild_client_settings() -> dict:
-    # Implement the function to rebuild the client settings.
-    pass
+
+def rebuild_client_settings(client: Client) -> dict:
+    # Get the current settings
+    current_settings = client.settings
+
+    # Generate new settings using the custom generate_new_settings function
+    new_settings = generate_new_settings(current_settings)
+
+    # Update the client's settings with the new values
+    client.settings = new_settings
+
+    # Save the new settings using the update_client_settings function
+    update_client_settings(client, new_settings)
+
+    return new_settings
+
+def generate_new_settings(existing_settings: dict) -> dict:
+    new_uuids = {
+        "phone_id": str(uuid.uuid4()),
+        "uuid": str(uuid.uuid4()),
+        "client_session_id": str(uuid.uuid4()),
+        "advertising_id": str(uuid.uuid4()),
+        "android_device_id": existing_settings["uuids"]["android_device_id"],
+        "request_id": str(uuid.uuid4()),
+        "tray_session_id": str(uuid.uuid4())
+    }
+
+    new_settings = {
+        "uuids": new_uuids,
+        "mid": existing_settings["mid"],
+        "ig_u_rur": existing_settings["ig_u_rur"],
+        "ig_www_claim": existing_settings["ig_www_claim"],
+        "authorization_data": existing_settings["authorization_data"],
+        "cookies": existing_settings["cookies"],
+        "last_login": existing_settings["last_login"],
+        "device_settings": existing_settings["device_settings"],
+        "user_agent": existing_settings["user_agent"],
+        "country": existing_settings["country"],
+        "country_code": existing_settings["country_code"],
+        "locale": existing_settings["locale"],
+        "timezone_offset": existing_settings["timezone_offset"]
+    }
+
+    return new_settings
 
 def freeze(message: str, hours: int = 0, days: int = 0) -> None:
     print(f"Freezing due to: {message}")
     freeze_time = hours * 3600 + days * 86400
     time.sleep(freeze_time)
+
+def next_proxy() -> str:
+    return "http://riccmpgq:nu5x1biz04h9@45.94.47.66:8110"
 
 
 def get_client(username: str, password: str) -> Union[Client, None]:
@@ -36,16 +81,21 @@ def get_client(username: str, password: str) -> Union[Client, None]:
         nonlocal username, password
         if isinstance(e, BadPassword):
             client.logger.exception(e)
-            raise ReloginAttemptExceeded(e)
+            client.set_proxy(next_proxy().href)
+            if client.relogin_attempt > 0:
+                freeze(str(e), days=7)
+                raise ReloginAttemptExceeded(e)
+
+            return update_client_settings(client, client.get_settings())
 
         elif isinstance(e, LoginRequired):
             client.logger.exception(e)
             client.login(username, password)
-            return update_client_settings(client.get_settings())
+            return update_client_settings(client, client.get_settings())
         elif isinstance(e, ChallengeRequired):
             api_path = client.last_json.get("challenge", {}).get("api_path")
             if api_path == "/challenge/":
-
+                client.set_proxy(next_proxy().href)
                 client.settings = rebuild_client_settings()
             else:
                 try:
@@ -75,4 +125,5 @@ def get_client(username: str, password: str) -> Union[Client, None]:
     cl = Client()
     cl.handle_exception = handle_exception
     cl.login(username, password)
+    cl.set_proxy(next_proxy())
     return cl
