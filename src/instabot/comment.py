abe983faab14c00ff3aaad5fd1c8a3ec7f2fa127
peer <peerdenhoed@hotmail.nl>
2023-05-18 15:35:04 +0200

    Comment on media function now comments on a single post per iteration and logs each comment, with a sleep time calculated based on the total number of comments to be made per day

--- src/instabot/comment.py
+++ src/instabot/comment.py
@@ -8,40 +8,22 @@ from instabot import utils
 from instabot import media
 
 async def comment_on_media(cl: Client, account: Dict[str, Any]):
-    try:
-        comment_on_tag = account['comment_on_media']['comment_on_tag']
-        amount_per_day = account['comment_on_media']['amount_per_day']
+    comment_on_tag = account['comment_on_media']['comment_on_tag']
+    amount_per_day = account['comment_on_media']['amount_per_day']
 
-        posts_to_comment = media.recent_medias_hashtag_no_auth(comment_on_tag, amount_per_day)
+    for i in range(1, amount_per_day):
+        utils.logger.info(f"Commenting on post { i } / {amount_per_day}")
 
-        if posts_to_comment:
-            for post in posts_to_comment:
-                try:
-                    comment = random.choice(account['comment_on_media']['comments'])
+        post = media.recent_medias_hashtag_no_auth(comment_on_tag, 1)
+        comment = random.choice(account['comment_on_media']['comments'])
 
-                    sleep_time = utils.calculate_sleep_time(amount_per_day)
-                    utils.logger.info(f"Sleeping for {sleep_time} before commenting")
-                    await asyncio.sleep(sleep_time)
+        cl.media_comment(post[0].id, comment)
 
-                    comment = cl.media_comment(post.id, comment)
+        sleep_time = utils.calculate_sleep_time(amount_per_day)
+        await asyncio.sleep(sleep_time)
 
-                    save_comment(comment.pk, account['username'])
-
-                    utils.logger.info(f"Commented on post {post.id}")
-                except Exception as e:
-                    utils.logger.error(f"Error while commenting on media {post['pk']}: {e}")
-                    # Continue with the next iteration without stopping the whole task
-                continue
-        else:
-            utils.logger.info(f"No posts to comment on")
-            return await asyncio.sleep(0)
-
-    except Exception as e:
-        utils.logger.error(f"Error while fetching media for hashtag {comment_on_tag}: {e}")
-         # Exit the current task without affecting other tasks
-        return await asyncio.sleep(0)
-
-
+## You could save a comment and delete it after a certain amount of time.
+## Deleting a comment not implemented yet.
 def save_comment(comment_pk: int, username: str):
     comment_folder = "./artifacts/logs/comments"
     os.makedirs(comment_folder, exist_ok=True)
