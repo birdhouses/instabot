c401e9117b37a270101bd2f8c724655206903b30
peer <peerdenhoed@hotmail.nl>
2023-06-17 15:47:09 +0200

    Add account frame and configure button to account widget in MainMenu

--- src/gui/menu.py
+++ src/gui/menu.py
@@ -27,21 +27,39 @@ class MainMenu(tk.Frame):
         self.canvas.grid(row=0, column=0, sticky="nsew")
         self.scrollbar.grid(row=0, column=1, sticky="ns")
 
-        # Add title
+         # Add title
         title_frame = tk.Frame(self.scrollable_frame, bg='black')
         title_frame.grid(sticky="ew")
         title_label = ctk.CTkLabel(title_frame, text="Accounts", font=("Arial", 30))
-        title_label.grid()
+        title_label.grid(pady=10)
 
-    def add_account(self, account):
+        # Add a separator line
+        separator = tk.Frame(self.scrollable_frame, bg='white', height=2)
+        separator.grid(sticky="ew")
+
+        # Add headers
+        headers_frame = tk.Frame(self.scrollable_frame, bg='black')
+        headers_frame.grid(sticky="ew")
+
+        username_header = ctk.CTkLabel(headers_frame, text="Username", font=("Arial", 20))
+        username_header.grid(row=0, column=0, padx=(10, 0))  # Left padding (margin) for the first column
+
+
+    def add_account(self, account, row):
         account_frame = tk.Frame(self.scrollable_frame, bg='black')
         account_frame.grid(sticky="ew")
+        account_frame._is_account_widget = True
 
         username_label = ctk.CTkLabel(account_frame, text=account['account_details']['username'])
-        username_label.grid(column=0)
+        username_label.grid(row=row, column=0, padx=(10, 0), pady=10)  # Left padding (margin) for the first column
 
         configure_button = ctk.CTkButton(account_frame, text="Configure", font=("Arial", 15), command=lambda: self.configure_account(account))
-        configure_button.grid(column=1)
+        configure_button.grid(row=row, column=1, padx=(10, 0), pady=10)
+
+        # Add a separator line
+        separator = tk.Frame(self.scrollable_frame, bg='white', height=2)
+        separator.grid(sticky="ew")
+        separator._is_account_widget = True
 
     def load_accounts_from_file(self):
         try:
@@ -54,8 +72,8 @@ class MainMenu(tk.Frame):
 
         accounts = config['accounts']
 
-        for account in accounts:
-            self.add_account(account)
+        for idx, account in enumerate(accounts):
+            self.add_account(account, idx + 1)
 
     def configure_account(self, account=None):
         main_screen.App(account=account, parent=self)
@@ -63,7 +81,8 @@ class MainMenu(tk.Frame):
     def refresh_accounts(self):
         # Clear current accounts
         for widget in self.scrollable_frame.winfo_children():
-            widget.destroy()
+            if hasattr(widget, '_is_account_widget'):
+                widget.destroy()
 
         # Reload accounts
         self.load_accounts_from_file()
@@ -75,6 +94,8 @@ class App():
         root = tk.Tk()
         root.configure(bg='black')
         root.title('Instagram Bot')
+
+        root.geometry('1000x700')
 
         root.grid_rowconfigure(0, weight=1)
         root.grid_columnconfigure(0, weight=1)
