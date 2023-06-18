e96348182c2e076c75dcbf5351ce8b763901d7c1
peer <peerdenhoed@hotmail.nl>
2023-06-18 18:41:45 +0200

    Add support for uploading videos in upload_media function

--- src/instabot/upload.py
+++ src/instabot/upload.py
@@ -21,12 +21,31 @@ async def upload_media(cl, account):
             upload_post(cl, media, posts_dir, caption, delete_after_upload)
         elif is_album(posts_dir, media):
             upload_album(cl, media, posts_dir, caption, delete_after_upload)
+        elif is_video(media):
+            upload_video(cl, media, posts_dir, caption, delete_after_upload)
         else:
             utils.logger.warning(f"{media} is not a valid post or album.")
             continue
 
         sleep_time = utils.calculate_sleep_time(amount)
         await asyncio.sleep(sleep_time)
+
+def is_video(media: str) -> bool:
+    return media.endswith('.mp4')
+
+def upload_video(cl, path, posts_dir, caption, delete_after_upload):
+    path_to_post = posts_dir + '/' + path
+
+    try:
+        cl.video_upload(path_to_post, caption)
+    except Exception as e:
+        utils.logger.error(f"Error uploading {path}: {e}")
+        return
+
+    if delete_after_upload:
+        os.remove(path_to_post)
+
+    utils.logger.info(f"Uploaded {path}")
 
 def is_album(posts_dir: str, path: str) -> bool:
     if not os.path.isdir(posts_dir + '/' + path):
